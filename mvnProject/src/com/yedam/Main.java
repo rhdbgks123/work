363a85d5de216cc3af5ccca39f3d0dd06801ea7c
YUHAN KO <rhdbgks789@gmail.com>
2025-07-25 10:45:05 +0900

    신규 프로젝트 추가

---
+++ mvnProject/src/com/yedam/Main.java
@@ -0,0 +1,22 @@
+package com.yedam;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.yedam.common.DBUtil;
+import com.yedam.vo.Book;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		DBUtil util = new DBUtil();
+		SqlSessionFactory factory = util.getInstance(); //DBUtil에 선언한 getInstance
+		SqlSession session = factory.openSession(); // sqlsession 객체반환
+		
+		Book book = session.selectOne("com.yedam.mapper.Bookmapper.selectBook", 1000);
+		
+		System.out.println(book);
+		
+		
+	}
+}
---
+++ mvnProject/src/com/yedam/vo/Book.java
@@ -0,0 +1,43 @@
+package com.yedam.vo;
+
+public class Book
+{
+	private int id;
+	private String title;
+	private String author;
+	private int prie;
+	
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public String getTitle()
+	{
+		return title;
+	}
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	public String getAuthor()
+	{
+		return author;
+	}
+	public void setAuthor(String author)
+	{
+		this.author = author;
+	}
+	public int getPrie()
+	{
+		return prie;
+	}
+	public void setPrie(int prie)
+	{
+		this.prie = prie;
+	}
+	
+}
