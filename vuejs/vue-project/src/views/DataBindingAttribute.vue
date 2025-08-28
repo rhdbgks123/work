7437d5d3fb203fbadd01baf679aa800c20de9f71
YUHAN KO <rhdbgks789@gmail.com>
2025-08-28 15:47:09 +0900

    0828

---
+++ vuejs/vue-project/src/views/DataBindingAttribute.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <img v-bind:src="imgSrc" v-bind:width="widthVal" />
+  </div>
+  <div class="container" v-bind:class="{ active: isActive, 'text-red': isRed }">
+    class binding
+  </div>
+  <div v-bind:class="clsAry">class binding 배열</div>
+  <div :style="backVal">Style Binding</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgSrc: "http://vuejs.org/images/logo.png",
+      widthVal: "100px",
+      isActive: false,
+      isRed: true,
+      activeCls: "active",
+      redCls: "text-red",
+      clsAry: ["active", "text-red"],
+      //   backVal: "background-color : red; color:green",
+      backVal: { backgroundColor: "red", color: "green" },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  background-color: green;
+  font-weight: bold;
+  color: white;
+}
+.text-red {
+  color: red;
+}
+</style>
